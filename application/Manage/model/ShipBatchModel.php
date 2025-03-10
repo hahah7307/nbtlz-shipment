e9f30541d22a2b8837248f0ac325951cd0689fa4
hahah7307 <1084429753@qq.com>
2025-03-10 14:25:35 +0800

    外销编号3-33：易仓头程出货单数据自动抓取(2)

--- application/Manage/model/ShipBatchModel.php
+++ application/Manage/model/ShipBatchModel.php
@@ -9,4 +9,29 @@ class ShipBatchModel extends Model
     protected $name = 'ecang_ship_batch';
 
     protected $resultSetType = 'collection';
+
+    public function packingInfo(): \think\model\relation\HasMany
+    {
+        return $this->hasMany('ShipBatchPackingInfoModel', 'ship_batch_id', 'id');
+    }
+
+    public function packingReceiving(): \think\model\relation\HasMany
+    {
+        return $this->hasMany('ShipBatchPackingReceivingModel', 'ship_batch_id', 'id');
+    }
+
+    public function dgOrder(): \think\model\relation\HasMany
+    {
+        return $this->hasMany('ShipBatchDgOrderInfoModel', 'ship_batch_id', 'id');
+    }
+
+    public function productInfo(): \think\model\relation\HasMany
+    {
+        return $this->hasMany('ShipBatchProductInfoModel', 'ship_batch_id', 'id');
+    }
+
+    public function receivingPurchase(): \think\model\relation\HasMany
+    {
+        return $this->hasMany('ShipBatchReceivingPurchaseModel', 'ship_batch_id', 'id');
+    }
 }
---
+++ application/Manage/view/ship/warehouse.php
@@ -0,0 +1,165 @@
+
+{include file="public/header" /}
+
+<!-- 主体内容 -->
+<div class="layui-body" id="LAY_app_body">
+    <div class="right">
+        <div class="title">海外仓头程明细列表</div>
+        <form class="layui-form search-form" method="get">
+            <div class="layui-inline w200">
+                <input type="text" class="layui-input" name="keyword" value="{$keyword}" placeholder="">
+            </div>
+            <div class="layui-inline w100">
+                <input type="text" class="layui-input" name="page_num" value="{$page_num}" placeholder="每页条数">
+            </div>
+            <div class="layui-inline">
+                <button class="layui-btn" lay-submit lay-filter="Search"><i class="layui-icon">&#xe615;</i> 查询</button>
+            </div>
+            <div class="layui-inline">
+                <a class="layui-btn layui-btn-normal" href="{:url('index')}"><i class="layui-icon">&#xe669;</i> 重置</a>
+            </div>
+        </form>
+
+		<div class="layui-form">
+			<table class="layui-table" lay-size="sm">
+				<colgroup>
+					<col>
+					<col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+					<col>
+				</colgroup>
+				<thead>
+					<tr>
+                        <th>order_code</th>
+                        <th>reference_no</th>
+                        <th>parcel_quantity</th>
+                        <th>remark</th>
+                        <th>stpo_code</th>
+                        <th>to_warehouse</th>
+                        <th>warehouse</th>
+                        <th>so_weight</th>
+                        <th>box_count</th>
+                        <th>amount</th>
+                        <th>user_account</th>
+                        <th>oab_state</th>
+                        <th>system_weight</th>
+                        <th>create_user</th>
+					</tr>
+				</thead>
+				<tbody>
+					{foreach name="list" item="v"}
+						<tr>
+							<td>{$v.order_code}</td>
+							<td>{$v.reference_no}</td>
+                            <td class="tr">{$v.parcel_quantity}</td>
+                            <td>{$v.remark}</td>
+                            <td>{$v.stpo_code}</td>
+                            <td>{$v.to_warehouse}</td>
+                            <td>{$v.warehouse}</td>
+                            <td class="tr">{$v.so_weight}</td>
+                            <td class="tr">{$v.box_count}</td>
+                            <td class="tr">{$v.amount}</td>
+                            <td>{$v.user_account}</td>
+                            <td>{$v.oab_state}</td>
+                            <td class="tr">{$v.system_weight}</td>
+                            <td>{$v.create_user}</td>
+						</tr>
+					{/foreach}
+				</tbody>
+			</table>
+            {$list->render()}
+		</div>
+
+    </div>
+</div>
+<script>
+layui.use(['form', 'jquery', 'laydate'], function(){
+    let $ = layui.jquery,
+        form = layui.form,
+        laydate = layui.laydate;
+
+    // 显示日期选择器
+    laydate.render({
+        elem: '#eta',
+        type: 'datetime'
+    });
+
+    // 排序
+	form.on('submit(Sort)', function(data){
+		var text = $(this).text(), button = $(this);
+		$('button').attr('disabled',true);
+		button.text('请稍候...');
+		$.ajax({
+			type:'POST',url:"{:url('sort')}",data:data.field,dataType:'json',
+			success:function(data){
+				if(data.code === 1){
+					layer.alert(data.msg,{icon:1,closeBtn:0,title:false,btnAlign:'c'},function(){
+						location.reload();
+					});
+				}else{
+					layer.alert(data.msg,{icon:2,closeBtn:0,title:false,btnAlign:'c'},function(){
+						layer.closeAll();
+						$('button').attr('disabled',false);
+						button.text(text);
+					});
+				}
+			}
+		});
+		return false;
+	});
+
+	// 状态
+	form.on('switch(formLock)', function(data){
+		$('button').attr('disabled',true);
+		$.ajax({
+			type:'POST',url:"{:url('status')}",data:{id:data.value,type:'look'},dataType:'json',
+			success:function(data){
+				if(data.code == 0){
+					layer.alert(data.msg,{icon:2,closeBtn:0,title:false,btnAlign:'c'},function(){
+						location.reload();
+					});
+				}
+			}
+		});
+	});
+
+	// 删除
+	form.on('submit(Detele)', function(data){
+		var text = $(this).text(),
+			button = $(this),
+			id = $(this).data('id');
+		layer.confirm('确定删除吗？',{icon:3,closeBtn:0,title:false,btnAlign:'c'},function(){
+			$('button').attr('disabled',true);
+			button.text('请稍候...');
+			$.ajax({
+				type:'POST',url:"{:url('delete')}",data:{id:id},dataType:'json',
+				success:function(data){
+					if(data.code == 1){
+						layer.alert(data.msg,{icon:1,closeBtn:0,title:false,btnAlign:'c'},function(){
+							location.reload();
+						});
+					}else{
+						layer.alert(data.msg,{icon:2,closeBtn:0,title:false,btnAlign:'c'},function(){
+							layer.closeAll();
+							$('button').attr('disabled',false);
+							button.text(text);
+						});
+					}
+				}
+			});
+		});
+	});
+});
+</script>
+
+{include file="public/footer" /}
